1707077a460a46993dc642269da1469200cf50be
rodrigo leonel duete <102498291+rodriduete@users.noreply.github.com>
2022-08-09 22:01:18 -0300

    Add files via upload

---
+++ comoquieras/js/index.js
@@ -0,0 +1,184 @@
+
+// IF / ELSE / ELSE IF
+
+
+//debugger
+
+//let username = "rodrigo";
+//let edad = 19;
+
+//if (username == "rodrigo") {
+    //console.log("bienvenido", username)
+//}
+
+
+//let nombre = prompt("Ingrese su nombre")
+
+//if(nombre == "rodrigo"){
+    //console.log("nombre desbloqueado")
+//} else{
+    //console.warn("nombre no reconocido")
+//}
+
+
+
+
+
+
+//debugger
+
+//let oferta = parseInt(prompt("Ingrese su oferta:"))//
+
+//if(oferta < 20){
+    //console.error("estas por abajo de lo pretendido");
+
+//} 
+
+//else if(oferta < 40){
+    //console.warn("te estas acercando");
+//}
+
+//else if(oferta >= 70){
+    //console.log("trato hecho");
+
+//}
+
+//debugger
+
+//let usuario = prompt("ingrese usuario");
+//let contraseña = prompt("ingrese contraseña");
+
+//if(usuario == "rodriduete" && contraseña == 12345){
+    //console.log("iniciaste secion")
+//}
+//else{
+    //console.warn("usuario y/o contraeña incorrecta")
+//}
+
+
+
+
+/*//Quieren comprar el helado mas caro que puedan con la plata que tienen.
+
+//roberto tiene $30
+//cristian tiene $50
+//bruno tiene $100
+
+// cucurucho $15
+// helado de crema $30
+// helado de agua $50
+// helado premium $100
+
+let dineroRoberto = parseInt(prompt("cuanto dinero tienes para gastar roberto?"))
+let dineroCristian = parseInt(prompt("cuanto dinero tienes para gastar cristian?"))
+let dineroBruno = parseInt(prompt("cuanto dinero tienes para gastar bruno?"))
+
+
+
+if(dineroRoberto >= 15 && dineroRoberto < 30){
+    alert("roberto,te podes comprar un cucurucho")
+    alert("y te sobran $" + (dineroRoberto - 15))
+}
+else if (dineroRoberto >=30 && dineroRoberto < 50){
+    alert("roberto,te podes comprar el helado de crema")
+    alert("y te sobran $" + (dineroRoberto - 30))
+}
+else if (dineroRoberto >=50 && dineroRoberto < 100){
+    alert("roberto,te podes comprar el helado de agua ")
+    alert("y te sobran $" + (dineroRoberto - 50))
+}
+else if (dineroRoberto >=100 && dineroRoberto < 150){
+    alert("roberto,te podes comprar el helado premium")
+    alert("y te sobran $" + (dineroRoberto - 100))
+}
+else if (dineroRoberto < 15){
+    alert("roberto,hacete un favor y sali del local pobre")
+}
+
+
+
+
+if(dineroCristian >= 15 && dineroCristian < 30){
+    alert("cristian,te podes comprar un cucurucho")
+    alert("y te sobran $" + (dineroCristian - 15))
+}
+else if (dineroCristian >=30 && dineroCristian < 50){
+    alert("cristian,te podes comprar el helado de crema")
+    alert("y te sobran $" + (dineroCristian - 30))
+}
+else if (dineroCristian >=50 && dineroCristian < 100){
+    alert("cristian,te podes comprar el helado de agua ")
+    alert("y te sobran $" + (dineroCristian - 50))
+}
+else if (dineroCristian >=100 && dineroCristian < 150){
+    alert("cristian,te podes comprar el helado premium")
+    alert("y te sobran $" + (dineroCristian - 100))
+}
+else if (dineroCristian < 15){
+    alert("cristian,hacete un favor y sali del local pobre")
+}
+
+
+
+if(dineroBruno >= 15 && dineroBruno < 30){
+    alert("bruno,te podes comprar un cucurucho")
+    alert("y te sobran $" + (dineroBruno - 15))
+}
+else if (dineroBruno >=30 && dineroBruno < 50){
+    alert("bruno,te podes comprar el helado de crema")
+    alert("y te sobran $" + (dineroBruno - 30))
+}
+else if (dineroBruno >=50 && dineroBruno < 100){
+    alert("bruno,te podes comprar el helado de agua ")
+    alert("y te sobran $" + (dineroBruno - 50))
+}
+else if (dineroBruno >=100 && dineroBruno < 150){
+    alert("bruno,te podes comprar el helado premium")
+    alert("y te sobran $" + (dineroBruno - 100))
+}
+
+else if (dineroBruno < 15){
+    alert("bruno,hacete un favor y sali del local pobre")
+}
+*/
+
+
+
+
+
+
+//  BUCLES: FOR
+
+//let i;
+
+//for(i=1;i<=10;i=i+1){
+   //console.log (i);
+//}
+
+
+
+let i;
+
+for(i=1;i<=100;i++){
+    if(i%5==0){
+    console.log(i);
+}
+}
+
+
+//ARRAYS
+
+// let producto = ["ropa","kiwi","banana","jean"]
+
+// document.write(producto[0])
+
+
+//ARRAYS ASOCIATIVOS
+
+// let conjunto = {
+//     remera:"blusa eva",
+//     abrigo:"campera carla",
+//     zapatos:"channel",
+//     pantalon:"palazzo"
+// };
+
